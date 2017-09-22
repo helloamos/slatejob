5a9e3ced3b304b1dcd678cc5ab342d607b8e9986
helloamos <salut.amos@gmail.com>
2017-09-22 22:46:29 +0000

    commit

--- config/puma_distant.rb
+++ config/puma_distant.rb
@@ -1,21 +1,21 @@
 #!/usr/bin/env puma
 
-directory '/home/rails/rails_project/slatejob'
-rackup "/home/rails/rails_project/slatejob/config.ru"
+directory '/home/rails/slatejob'
+rackup "/home/rails/slatejob/config.ru"
 environment 'production'
 
 tag ''
 
-pidfile "/home/rails/rails_project/slatejob/shared/tmp/pids/puma.pid"
-state_path "/home/rails/rails_project/slatejob/shared/tmp/pids/puma.state"
-stdout_redirect '/home/rails/rails_project/slatejob/shared/log/puma_access.log', '/home/rails/rails_project/slatejob/shared/log/puma_error.log', true
+pidfile "/home/rails/slatejob/shared/tmp/pids/puma.pid"
+state_path "/home/rails/slatejob/shared/tmp/pids/puma.state"
+stdout_redirect '/home/rails/slatejob/shared/log/puma_access.log', '/home/rails/slatejob/shared/log/puma_error.log', true
 
 
 threads 0,16
 
 
 
-bind 'unix:///home/rails/rails_project/slatejob/shared/tmp/sockets/puma.sock'
+bind 'unix:///home/rails/slatejob/shared/tmp/sockets/puma.sock'
 
 workers 1
 
@@ -28,7 +28,7 @@ prune_bundler
 
 on_restart do
   puts 'Refreshing Gemfile'
-  ENV["BUNDLE_GEMFILE"] = "/home/root/rails_projects/slatejob/current/Gemfile"
+  ENV["BUNDLE_GEMFILE"] = "/home/rails/slatejob/current/Gemfile"
 end
 
 
