fea3e5a77f3294c615e7c5339e2add69c122115d
helloamos <salut.amos@gmail.com>
2017-11-26 11:20:53 +0000

    Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,7 @@ Rails.application.configure do
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
 
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
@@ -32,6 +33,12 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+
+  config.serve_static_assets = true
+
+  #me
+    config.assets.digest = true
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
