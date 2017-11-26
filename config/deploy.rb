557f4937901cd866bf50ad3a8607e201ce2e5378
helloamos <salut.amos@gmail.com>
2017-11-26 02:21:29 +0000

    Important

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,6 +37,7 @@ set :puma_workers,1
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { rvm_bin_path: '~/.rvm/bin' }
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
