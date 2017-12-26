3cf8f870e95ceafafd2747a1da4d0cc6c40567a8
helloamos <salut.amos@gmail.com>
2017-12-26 12:39:45 +0000

    Update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server "165.227.69.28", user: "rails", roles: %w{app db web}, ssh_options: {forward_agent: true}
+server "165.227.69.28", user: "root", roles: %w{app db web}, ssh_options: {forward_agent: true}
 
 
 
