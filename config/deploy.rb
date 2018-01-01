8c902cb47385ebe0860ce5c834a8b5f7eeb40655
helloamos <salut.amos@gmail.com>
2018-01-01 18:43:45 +0000

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.10.0"
 
 set :application, "staging.slatejob.com"
-set :repo_url, "https://github.com/helloamos/slatejob"
+set :repo_url, "https://gitlab.com/thekernel/slatejob"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
